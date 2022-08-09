3641b61c5cebada6693887f21ff89b41997ed999
Maritxx <maritdevries1@live.nl>
2022-08-09 11:56:29 +0200

    Changes function validate

--- main.js
+++ main.js
@@ -31,24 +31,60 @@ $(window).on("load resize orientationchange", function () {
 
 
 
-$("button#signupButton").click(function () {
-	validate();
-});
+const validateEmail = (email) => {
+   //Is a function expression, function is stored in variable.
+   //The (email) is another variable (this is the id from input in HTML).
+  
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+  //Checks if the string (stored in the email variable) matches the regular expression.
+  //Returns the result (does it match, or does it not match? true or false)
 
 
 
-function validate() {
-	var input = $("input#email").val();
-	var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const validate = () => {
 
-	if input.match(email) {
-		$("span#invalidSubmit").text("");
-	}
+  const result = document.getElementById('invalidSubmit');
+  //Sets the variable 'result' (this is the id from the span that will show on invalid input).
 
-	else {
-		$("span#invalidSubmit").text("Please input a valid email");
-	}
+  const email = document.getElementById('email').value;
+  //Sets the variable 'email' and makes it so that it will return the submitted input from the form.
 
-	console.log("input");
-	console.log("this function is working")	
+  result.innerHTML = '';
+  //Unsets the value of the span.
+  
+
+
+  if (email == '') {
+  	result.innerHTML = 'this field can not be empty';
+  	//Adds a string. 
+
+  	result.style.display="block";
+  	
+  	//Sets the display of the span to block, making it visbible.
+  } else if (validateEmail(email)) {
+  //If the input matches the regular expression.
+  
+  result.style.display="none";
+  //Sets the display of the span to 'none', making it invisible.
+  }
+
+    else {
+     result.innerHTML = email + ' is not valid';
+     //Takes the value from email and adds string 'is not valid'. 
+
+     result.style.display="block"; 
+     //Sets the display of the span to block, making it visbible.
+  }
+  return false;
+  //Makes it so the form does not submit when the email isn't formatted correctly.
 }
+
+
+const signupButton = document.getElementById("signupButton");
+  //Sets variable for submit button of form.
+
+signupButton.addEventListener("click", validate);
+//Makes it run the function 'validate' when submit button is clicked.
